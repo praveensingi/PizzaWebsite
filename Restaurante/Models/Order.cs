3be65b32225cdee83bed2c086eb862454abe7bcc
shipleydm <shipley17dm@gmail.com>
2022-09-30 16:35:01 -0400

    validations, annotation, and improved Order pages

--- Restaurante/Models/Order.cs
+++ Restaurante/Models/Order.cs
@@ -1,21 +1,31 @@
-﻿namespace Restaurante.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace Restaurante.Models
 {
     public class Order
     {
         public int Id { get; set; }
 
+        [Required()]
         public int Quantity { get; set; }
 
+        [Required()]
+        [DisplayName("Order Date")]
+        [DataType(DataType.Date)]
         public DateTime OrderDate { get; set; }
 
+        [DisplayName("Total Cost")]
         public decimal TotalCost { get; set; }
 
         public Customer? Customer { get; set; }
 
+        [DisplayName("Customer Name")]
         public int CustomerId { get; set; }
 
         public Staff? Staff { get; set; }
 
+        [DisplayName("Staff Name")]
         public int? StaffId { get; set; }
 
         public List<OrderFoodItem>? OrderFoodItems { get; set; }
