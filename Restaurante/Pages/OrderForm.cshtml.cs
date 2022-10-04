e83157045faebf074dbc1e9c160ebb2c2356b250
Singi Praveen <singipraveen999@gmail.com>
2022-10-04 09:13:43 +0530

    Added Grid functionality & footer

--- Restaurante/Pages/OrderForm.cshtml.cs
+++ Restaurante/Pages/OrderForm.cshtml.cs
@@ -1,10 +1,21 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Restaurante.Data;
+using Restaurante.Models;
 
 namespace Restaurante.Pages
 {
     public class OrderFormModel : PageModel
     {
+        private readonly Restaurante.Data.ApplicationDbContext _context;
+
+        public List<FoodItem>? FoodItems { get; set; }
+
+        public OrderFormModel(Restaurante.Data.ApplicationDbContext context)
+        {
+            _context = context;
+            FoodItems = _context.FoodItem.ToList();
+        }
         public void OnGet()
         {
         }
